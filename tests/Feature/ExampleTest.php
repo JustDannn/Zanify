158e1a98f5a26637ecdd78a5197ceafcb0eac51e
justdann <daninurfatah@gmail.com>
2025-12-12 08:13:53 +0700

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,10 @@
 <?php
 
 test('returns a successful response', function () {
-    $response = $this->get('/');
+    // 1. Buat user palsu (menggunakan factory)
+    $user = User::factory()->create();
 
+    // 2. Gunakan actingAs() untuk berpura-pura user sudah login
+    $response = $this->actingAs($user)->get('/');
     $response->assertStatus(200);
 });
